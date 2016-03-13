7a9263a5b039a9d32e8cd835f333fa14f49347a5
Rahul Kanojia <kaanorahul8@gmail.com>
2016-03-13 12:33:21 -0700

    Revert "Added nav drawer"

--- app/src/main/java/org/acm/nitsurat/digitalcanteen/MainActivity.java
+++ app/src/main/java/org/acm/nitsurat/digitalcanteen/MainActivity.java
@@ -2,19 +2,14 @@ package org.acm.nitsurat.digitalcanteen;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,25 +26,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         .setAction("Action", null).show();
             }
         });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
@@ -72,29 +48,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.new_order) {
-            Toast.makeText(getApplicationContext(),"Yopu pressed new order button", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.menu_favourtes) {
-            Toast.makeText(getApplicationContext(), "Yopu pressed orders button", Toast.LENGTH_SHORT).show();
-
-        } else if (id == R.id.menu_wallet) {
-            Toast.makeText(getApplicationContext(), "Yopu pressed wallet button", Toast.LENGTH_SHORT).show();
-
-        } else if (id == R.id.menu_setting) {
-            Toast.makeText(getApplicationContext(), "Yopu pressed settings button", Toast.LENGTH_SHORT).show();
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
